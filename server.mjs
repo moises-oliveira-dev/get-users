720f648a7db2b381a2ef28dae55adee6cc24c5e6
Moisés <moisesolivmendes@gmail.com>
2026-07-16 21:04:57 -0300

    feat: Adicionar interface para o recurso de usurioa

--- server.mjs
+++ server.mjs
@@ -1,5 +1,26 @@
 import http from 'node:http';
 
+const users = [{
+    name: 'Moises',
+    age: 15,
+    status: true
+}, {
+    name: 'Gaymar',
+    age: 300,
+    status: false
+}, {
+    name: 'PapaiVina',
+    age: 2,
+    status: true
+}];
+
 http.createServer((request, response) => {
-    response.end('Hellor Client');
+    if (request.url === '/users') {
+        response.writeHead(200, {'content-type': 'application/json'});
+        response.end(JSON.stringify(users));
+    } else {
+        response.writeHead(200, {'content-type': 'application/json'});
+        response.end(JSON.stringify({mesage: 'Pagina não encontrada'}));
+    }
+    
 }).listen(3000);
