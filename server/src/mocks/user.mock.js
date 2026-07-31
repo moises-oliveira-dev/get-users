939fc4a32b42e1a673f48818a0c9db48ac798f1c
Moisés <moisesolivmendes@gmail.com>
2026-07-30 21:41:03 -0300

    feat: adicionar estrutura de busca de usuarios a partir de entratada de dados no navegador

---
+++ server/src/mocks/user.mock.js
@@ -0,0 +1,18 @@
+const users = [{
+    id: 1,
+    name: 'ana',
+    age: 30,
+    status: true
+}, {
+    id: 2,
+    name: 'Maria',
+    age: 31,
+    status: true
+}, {
+    id: 3,
+    name: 'Joao',
+    age: 23,
+    status: false
+}];
+
+export default users;
