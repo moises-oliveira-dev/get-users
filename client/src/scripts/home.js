ca528b3699ebd711d197687f1811472765653948
Moises <moisesolivmendes@gmail.com>
2026-08-13 21:38:30 -0300

    feat: Criação do js para fazer adição de funcionalidade do home, fazendo utilização do setTimeout e addEventListener

---
+++ client/src/scripts/home.js
@@ -0,0 +1,16 @@
+const button = document.querySelector('button');
+const title = document.querySelector('h1');
+
+button.addEventListener ('click', () => {
+    const value = confirm('Deseja realmente sair?');
+
+    if (!value) {
+        return
+    }
+    button.style.display = 'none';
+    title.textContent = 'Saindo...';
+
+  setTimeout (() =>{
+    window.location.href = './login.html';
+  }, 3000);
+});
